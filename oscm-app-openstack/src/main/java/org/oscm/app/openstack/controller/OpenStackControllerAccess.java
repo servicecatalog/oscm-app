1310df0aa0fa86765217a53f9c79ff038bb05726
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2019-03-01 14:53:11 +0100

    Added initial module for openstack component

---
+++ oscm-app-openstack/src/main/java/org/oscm/app/openstack/controller/OpenStackControllerAccess.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ *                                                                              
+ *  Copyright FUJITSU LIMITED 2018
+ *                                                                              
+ *  Creation Date: 03.07.2014                                                      
+ *                                                                              
+ *******************************************************************************/
+
+package org.oscm.app.openstack.controller;
+
+import org.oscm.app.openstack.i18n.Messages;
+import org.oscm.app.v2_0.intf.ControllerAccess;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class OpenStackControllerAccess implements ControllerAccess {
+
+    private static final long serialVersionUID = 8912611326487987648L;
+
+    @Override
+    public String getControllerId() {
+        return OpenStackController.ID;
+    }
+
+    @Override
+    public String getMessage(String locale, String key, Object... arguments) {
+        return Messages.get(locale, key, arguments);
+    }
+
+    @Override
+    public List<String> getControllerParameterKeys() {
+        LinkedList<String> result = new LinkedList<>();
+        result.add(PropertyHandler.API_USER_NAME);
+        result.add(PropertyHandler.API_USER_PWD);
+        result.add(PropertyHandler.KEYSTONE_API_URL);
+        result.add(PropertyHandler.TENANT_ID);
+        result.add(PropertyHandler.DOMAIN_NAME);
+        result.add(PropertyHandler.TEMPLATE_BASE_URL);
+        result.add(PropertyHandler.TIMER_INTERVAL);
+        return result;
+    }
+}
