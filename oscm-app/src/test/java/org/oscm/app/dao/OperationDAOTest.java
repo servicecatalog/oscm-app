07dd712097632ab16fd3b90fbc1ba08dac791a2d
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2020-10-29 11:54:57 +0100

    DAO classes unit tests (#103)
    * SI dao tests
    
    * Added test for other DAOs

--- oscm-app/src/test/java/org/oscm/app/dao/OperationDAOTest.java
+++ oscm-app/src/test/java/org/oscm/app/dao/OperationDAOTest.java
@@ -1,67 +1,107 @@
-/*******************************************************************************
- *  Copyright FUJITSU LIMITED 2018
- *******************************************************************************/
-
+/**
+ * ***************************************************************************** Copyright FUJITSU
+ * LIMITED 2018 *****************************************************************************
+ */
 package org.oscm.app.dao;
-
-import static org.junit.Assert.assertNull;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
-
-import java.util.Properties;
-
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.Query;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import org.oscm.app.domain.Operation;
 import org.oscm.app.domain.ServiceInstance;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
+
 public class OperationDAOTest {
 
-    private static OperationDAO opDAO = spy(new OperationDAO());
-    private static EntityManager em = mock(EntityManager.class);
+  private static OperationDAO opDAO = spy(new OperationDAO());
+  private static EntityManager em = mock(EntityManager.class);
 
-    @Before
-    public void setup() {
-        opDAO.em = em;
-        doNothing().when(em).persist(anyObject());
-        doReturn(mock(Query.class)).when(em).createNamedQuery(anyString());
-    }
+  private Query query = mock(Query.class);
 
-    @Test
-    public void addOperationForQueue() {
-        // given
-        Operation op = new Operation();
-        doReturn(op).when(opDAO).createOperationForQueue(
-                any(ServiceInstance.class), any(Properties.class), anyString());
+  @Before
+  public void setup() {
+    opDAO.em = em;
+    doNothing().when(em).persist(anyObject());
+    doReturn(query).when(em).createNamedQuery(anyString());
+  }
 
-        // when
-        opDAO.addOperationForQueue(new ServiceInstance(), new Properties(),
-                new String());
+  @Test
+  public void addOperationForQueue() {
+    // given
+    ServiceInstance instance = new ServiceInstance();
+    Properties opParameters = new Properties();
+    String transactionId = "tId";
 
-        // then
-        verify(em).persist(op);
-    }
+    // when
+    Operation operation = opDAO.addOperationForQueue(instance, opParameters, transactionId);
 
-    @Test
-    public void getOperation_emptyQueue() {
-        // given
-        doThrow(new NoResultException()).when(opDAO).createOperationForQueue(
-                any(ServiceInstance.class), any(Properties.class), anyString());
+    // then
+    assertEquals(instance, operation.getServiceInstance());
+    assertEquals(opParameters, operation.getParametersAsProperties());
+    assertEquals(transactionId, operation.getTransactionId());
+    assertTrue(operation.isForQueue());
+    verify(em).persist(operation);
+  }
 
-        // when
-        assertNull(opDAO.getOperationFromQueue("anyInstanceId"));
-    }
+  @Test
+  public void addOperation() {
+    // given
+    ServiceInstance instance = new ServiceInstance();
+    Properties opParameters = new Properties();
+    String transactionId = "tId";
 
+    // when
+    Operation operation = opDAO.addOperation(instance, opParameters, transactionId);
+
+    // then
+    assertEquals(instance, operation.getServiceInstance());
+    assertEquals(opParameters, operation.getParametersAsProperties());
+    assertEquals(transactionId, operation.getTransactionId());
+    assertFalse(operation.isForQueue());
+    verify(em).persist(operation);
+  }
+
+  @Test
+  public void testGetOperationFromQueue() {
+    // given
+    String instanceId = "instanceId";
+    Operation operation = new Operation();
+    when(query.getSingleResult()).thenReturn(operation);
+
+    // when
+    Operation operationFromQueue = opDAO.getOperationFromQueue(instanceId);
+
+    // then
+    assertEquals(operation, operationFromQueue);
+  }
+
+  @Test
+  public void testGetOperationByInstanceId() {
+    // given
+    String instanceId = "instanceId";
+    Operation operation = new Operation();
+    when(query.getSingleResult()).thenReturn(operation);
+
+    // when
+    Operation operationByInstanceId = opDAO.getOperationByInstanceId(instanceId);
+
+    // then
+    assertEquals(operation, operationByInstanceId);
+  }
+
+  @Test
+  public void tesRemoveOperation() {
+    // when
+    opDAO.removeOperation(1000);
+
+    // then
+    verify(query, times(1)).executeUpdate();
+  }
 }
