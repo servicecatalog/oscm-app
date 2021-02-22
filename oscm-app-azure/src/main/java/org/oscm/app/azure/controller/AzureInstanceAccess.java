fce9563b4dd32f154cd5f8efd5b7ccb40fe97727
cworf91 <christian.worf@est.fujitsu.com>
2021-02-22 12:26:56 +0100

    implement status tab

--- oscm-app-azure/src/main/java/org/oscm/app/azure/controller/AzureInstanceAccess.java
+++ oscm-app-azure/src/main/java/org/oscm/app/azure/controller/AzureInstanceAccess.java
@@ -1,12 +1,21 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  *
- *  Copyright FUJITSU LIMITED 2018
+ * <p>Copyright FUJITSU LIMITED 2018
  *
- *  Creation Date: 2018-10-01
+ * <p>Creation Date: 2018-10-01
  *
- *******************************************************************************/
+ * <p>*****************************************************************************
+ */
 package org.oscm.app.azure.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.oscm.app.azure.AzureCommunication;
+import org.oscm.app.azure.data.AzureServer;
 import org.oscm.app.azure.i18n.Messages;
 import org.oscm.app.v2_0.APPlatformServiceFactory;
 import org.oscm.app.v2_0.data.ProvisioningSettings;
@@ -17,42 +26,53 @@ import org.oscm.app.v2_0.intf.APPlatformService;
 import org.oscm.app.v2_0.intf.InstanceAccess;
 import org.oscm.app.v2_0.intf.ServerInformation;
 
-import javax.annotation.PostConstruct;
-import java.util.Collections;
-import java.util.List;
-
 public class AzureInstanceAccess implements InstanceAccess {
-    // Reference to an APPlatformService instance
-    private APPlatformService platformService;
+  // Reference to an APPlatformService instance
+  private APPlatformService platformService;
 
-    private static final long serialVersionUID = 120266701249164759L;
+  private static final long serialVersionUID = 120266701249164759L;
 
-    /**
-     * Retrieves an <code>APPlatformService</code> instance.
-     * <p>
-     * The <code>APPlatformService</code> provides helper methods by which the
-     * service controller can access common APP utilities, for example, send
-     * emails or lock application instances.
-     */
-    @PostConstruct
-    public void initialize() {
-        platformService = APPlatformServiceFactory.getInstance();
-    }
+  /**
+   * Retrieves an <code>APPlatformService</code> instance.
+   *
+   * <p>The <code>APPlatformService</code> provides helper methods by which the service controller
+   * can access common APP utilities, for example, send emails or lock application instances.
+   */
+  @PostConstruct
+  public void initialize() {
+    platformService = APPlatformServiceFactory.getInstance();
+  }
 
-    @Override
-    public List<? extends ServerInformation> getServerDetails(String instanceId, String subscriptionId, String organizationId) throws AuthenticationException, ConfigurationException, APPlatformException {
-        return Collections.emptyList();
-    }
+  @Override
+  public List<? extends ServerInformation> getServerDetails(
+      String instanceId, String subscriptionId, String organizationId)
+      throws AuthenticationException, ConfigurationException, APPlatformException {
 
-    @Override
-    public String getAccessInfo(String instanceId, String subscriptionId, String organizationId) throws AuthenticationException, ConfigurationException, APPlatformException {
-        ProvisioningSettings settings = platformService.getServiceInstanceDetails(
-                AzureController.ID, instanceId, subscriptionId, organizationId);
-        return settings.getServiceAccessInfo();
-    }
+    ProvisioningSettings settings =
+        platformService.getServiceInstanceDetails(
+            AzureController.ID, instanceId, subscriptionId, organizationId);
+    PropertyHandler ph = new PropertyHandler(settings);
 
-    @Override
-    public String getMessage(String locale, String key, Object... arguments) {
-        return Messages.get(locale, key, arguments);
-    }
+    AzureCommunication azureCom = new AzureCommunication(ph);
+
+    List<AzureServer> servers = new ArrayList<>();
+    AzureServer server = azureCom.getAzureServerInfo(instanceId);
+    servers.add(server);
+
+    return servers;
+  }
+
+  @Override
+  public String getAccessInfo(String instanceId, String subscriptionId, String organizationId)
+      throws AuthenticationException, ConfigurationException, APPlatformException {
+    ProvisioningSettings settings =
+        platformService.getServiceInstanceDetails(
+            AzureController.ID, instanceId, subscriptionId, organizationId);
+    return settings.getServiceAccessInfo();
+  }
+
+  @Override
+  public String getMessage(String locale, String key, Object... arguments) {
+    return Messages.get(locale, key, arguments);
+  }
 }
